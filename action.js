05556482ebb5fd82045093af304d60cf8f618ace
Shaquib Ahmad Khan <shaquibkhan@Shaquibs-MacBook-Air.local>
2024-02-08 15:57:04 +0530

    updated work experience & project link

--- action.js
+++ action.js
@@ -1,7 +1,16 @@
 // Grab HTML Elements
+
+const myBody = document.querySelector('body');
 const btn = document.querySelector("button.mobile-menu-button");
 const menu = document.querySelector(".mobile-menu");
 
 btn.addEventListener("click", () => {
 	menu.classList.toggle("hidden");
 });
+
+const darkMode = document.querySelector('.dark-mode');
+
+darkMode.addEventListener('click', ()=>{
+	myBody.style.backgroundColor = 'red';
+	console.log('Dark mode')
+})
